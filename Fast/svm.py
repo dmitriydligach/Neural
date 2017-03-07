62b87a0f32e57aa591f21986a18ab8ef5fda4d8d
dimdym <dimdym@gmail.com>
2017-03-07 10:05:59 -0600

    added grid search using cv on train set

--- Fast/svm.py
+++ Fast/svm.py
@@ -1,13 +1,14 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python -B
 
 import glob, string, ConfigParser, sys, os
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.grid_search import GridSearchCV
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 def load_data(path):
   """Read data from file; return examples and labels"""
@@ -43,9 +44,30 @@ def train_and_test(train_file, test_file):
            labels=['contains', 'contains-1'], average='micro')
   print 'f1(contains & contains-1 micro average) = ', f1
 
+def grid_search(train_file, test_file):
+  """Run a grid search"""
+
+  train_samples, train_targets = load_data(train_file)
+  test_samples, test_targets = load_data(test_file)
+
+  vectorizer = CountVectorizer(ngram_range=(1,3))
+  train_counts = vectorizer.fit_transform(train_samples)
+  test_counts = vectorizer.transform(test_samples)
+
+  tf = TfidfTransformer()
+  train_tfidf = tf.fit_transform(train_counts)
+  test_tfidf = tf.transform(test_counts)
+
+  classifier = LinearSVC(class_weight='balanced')
+  params = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+  gs = GridSearchCV(estimator=classifier,
+                    param_grid=params,
+                    scoring='accuracy',
+                    cv=2)
+  gs.fit(train_tfidf, train_targets)
+
 if __name__ == "__main__":
 
-  # settings file specified as command-line argument
   cfg = ConfigParser.ConfigParser()
   cfg.read(sys.argv[1])
   base = os.environ['DATA_ROOT']
@@ -54,4 +76,5 @@ if __name__ == "__main__":
   print 'train:', train_file
   print 'test:', test_file
 
-  train_and_test(train_file, test_file)
+  # train_and_test(train_file, test_file)
+  grid_search(train_file, test_file)
