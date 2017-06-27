1b604efc703ef14e9db52be3f5ad5957f7c61a8d
dimdym <dimdym@gmail.com>
2017-06-27 11:07:27 -0500

    added svm training

--- Patient/ft.py
+++ Patient/ft.py
@@ -8,15 +8,10 @@ sys.path.append('../Lib/')
 sys.dont_write_bytecode = True
 import ConfigParser, os, numpy
 import keras as k
+import sklearn as sk
+from keras.preprocessing.sequence import pad_sequences
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
-from keras.utils.np_utils import to_categorical
-from keras.optimizers import RMSprop
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.layers import GlobalAveragePooling1D
-from keras.layers.embeddings import Embedding
 from keras.models import load_model
 from keras.models import Model
 import dataset
@@ -34,18 +29,20 @@ if __name__ == "__main__":
   dataset = dataset.DatasetProvider(data_dir)
   x, y = dataset.load()
   x = pad_sequences(x, maxlen=MAXLEN)
-  print 'x shape:', x.shape
+  print 'source x shape:', x.shape
   print 'y len:', len(y)
 
   # make vectors for target task
   model = load_model(MODEL_PATH)
   interm_layer_model = Model(inputs=model.input,
                              outputs=model.get_layer('ptvec').output)
-  interm_output = interm_layer_model.predict(x)
-  print 'x shape:', interm_output.shape
-
-  # save vectors for debugging
-  numpy.savetxt('data.txt', interm_output)
+  x = interm_layer_model.predict(x)
+  print 'target x shape:', x.shape
 
   # conduct svm training
-  
+  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+  classifier = sk.svm.LinearSVC(class_weight='balanced')
+  model = classifier.fit(x_train, y_train)
+  predicted = classifier.predict(x_test)
+  f1 = f1_score(y_test, predicted, pos_label=1)
+  print 'f1 =', f1
